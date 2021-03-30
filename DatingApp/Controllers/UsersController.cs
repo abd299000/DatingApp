7a43a6cef98306e815961a7e3122a9417b351e04
Muhammad Abdullah <80218765+abd299000@users.noreply.github.com>
2021-03-31 00:07:19 +0500

    Initial commit

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("datingapp/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext Configration;
+
+        public UsersController(DataContext config)
+
+        {
+            Configration = config;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUser()
+        {
+            return Configration.Users.ToList();
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return Configration.Users.Find(id);
+
+        }
+    }
+}
---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+//0using System.Data.Entity;
+//using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
